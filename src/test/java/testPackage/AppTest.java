f1fd88c46943f71b7bb919706695ed4e1b28000d
vinny160 <DELL@DESKTOP-2VFFO01>
2020-01-06 21:49:16 +0530

    learning_1

---
+++ src/test/java/testPackage/AppTest.java
@@ -0,0 +1,11 @@
+package testPackage;
+
+import org.testng.annotations.Test;
+
+public class AppTest {
+	 @Test
+	    public void test() {
+	        //Assert.assertEquals("Hello Maven", new String("Hello Maven"));
+		 System.out.println("Test code work check");
+	    }
+}
