ff121699b2fdabd78156f4e9c7e60e07eafd3d8b
vinny160 <DELL@DESKTOP-2VFFO01>
2020-01-06 21:53:49 +0530

    adding a new test case

---
+++ src/test/java/testPackage/AppTest1.java
@@ -0,0 +1,11 @@
+package testPackage;
+
+import org.testng.annotations.Test;
+
+public class AppTest1 {
+	 @Test
+	    public void test() {
+	        //Assert.assertEquals("Hello Maven", new String("Hello Maven"));
+		 System.out.println("Test code work check again");
+	    }
+}
