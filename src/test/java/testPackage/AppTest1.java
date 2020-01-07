678fd81fae381f8b00fda80be1b24b17cdfb6bae
vinny160 <DELL@DESKTOP-2VFFO01>
2020-01-07 21:56:20 +0530

    testing

--- src/test/java/testPackage/AppTest1.java
+++ src/test/java/testPackage/AppTest1.java
@@ -6,6 +6,6 @@ public class AppTest1 {
 	 @Test
 	    public void test() {
 	        //Assert.assertEquals("Hello Maven", new String("Hello Maven"));
-		 System.out.println("Test code work check again");
+		 System.out.println("Test code work check again1");
 	    }
 }
